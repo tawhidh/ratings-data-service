c391bc516cb743ab5abc3f3d68ae87790a525f8a
tawhid.hassan <tawhid.tirzok@gmail.com>
2023-10-24 22:39:25 +0600

    created new service

---
+++ src/main/java/com/tawhid/ratingsdataservice/controller/RatingsDataResource.java
@@ -0,0 +1,16 @@
+package com.tawhid.ratingsdataservice.controller;
+
+import com.tawhid.ratingsdataservice.model.Rating;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsDataResource {
+    @GetMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+}
---
+++ src/main/java/com/tawhid/ratingsdataservice/model/Rating.java
@@ -0,0 +1,27 @@
+package com.tawhid.ratingsdataservice.model;
+
+public class Rating {
+    private String movieId;
+    private Integer rating;
+
+    public Rating(String movieId, Integer rating) {
+        this.movieId = movieId;
+        this.rating = rating;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+}
